c15a8726bde6be02ec3f6d8bf9553c9c367dce18
Julio Montenegro <jmontenegro@hexacta.com>
2023-05-25 16:10:12 +0200

    AccountController API

---
+++ HealthyApp/HealthyApp.API/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyApp.API.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IUserStore<IdentityUser> _userStore;
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
